468b46705c2cd84eac8e6e2d3cc2144fd20a3b46
awanjila <abemuchikan@gmail.com>
2018-09-26 18:55:41 +0300

    upload and show excel document

---
+++ views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+import openpyxl
+
+#This is here communicates with then index that is being relayed in urls.py
+def index(request):
+    if "GET" == request.method:
+        return render(request, 'uploadReadEXcel/templates/index.html', {})
+    else:
+        excel_file=request.FILES["excel_file"]
+    #I will put validations here to check extension or filesize
+        wb=openpyxl.load_workbook(excel_file)
+
+    # getting a particular sheet by name out of many sheets
+        worksheet=wb["Sheet1"]
+
+    print(worksheet)
+
+    excel_data=list()
+    #iterating over rows and
+    #getting value from each cell in row
+    for row in worksheet.iter_rows():
+
+        row_data=list()
+
+    for cell in row:
+       row_data.append(str(cell.value))
+    excel_data.append(row_data)
+
+    return render(request, 'uploadReadEXcel/templates/index.html', {"excel_data":excel_data})
